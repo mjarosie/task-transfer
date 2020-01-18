15e4cb787f34ce3656318b4887ec8e1648c98354
Lex <lex@dhcp-18-21-149-114.dyn.mit.edu>
2020-01-17 20:52:22 -0500

    Lex Commit 1

--- Assets/MagicLeap/Examples/Scripts/Visualizers/VideoCaptureVisualizer.cs
+++ Assets/MagicLeap/Examples/Scripts/Visualizers/VideoCaptureVisualizer.cs
@@ -70,6 +70,7 @@ namespace MagicLeap
             _mediaPlayer.OnVideoPrepared += HandleVideoPrepared;
 
             _screenRenderer = _screen.GetComponent<Renderer>();
+			_screenRenderer.enabled = false;
         }
 
         void OnDestroy()
